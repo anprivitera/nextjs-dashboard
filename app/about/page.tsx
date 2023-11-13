0493d9a7e3567bf98e7f3bc9ce11cb0f5e15bcb5
Andrea Privitera <andrea.privitera@7gen.com>
2023-11-12 22:14:11 -0800

    created sample about page

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+export default function About() {
+    function getDate() {
+        return new Date().toLocaleDateString();
+    }
+    return (
+        <>
+            <h1>Welcome! Today is { getDate() }</h1>
+            <h2>Here are some links to get you started:</h2>
+        </>
+    )
+}
