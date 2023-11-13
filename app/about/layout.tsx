ba1628e339ce2e9893bce83ae4a524c58c4193e3
Andrea Privitera <andrea.privitera@7gen.com>
2023-11-12 22:57:43 -0800

    code formatting

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={ `${lusitana.className} antialiased `}>  {children}</body>
+            <body className={`${lusitana.className} antialiased `}>  {children}</body>
             {/* Prop `className` did not match. Server: "__className_e66fe9 antialiased" Client: "__className_712214 antialiased */}
         </html>
     );
