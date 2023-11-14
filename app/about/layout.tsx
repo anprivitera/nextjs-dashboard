3af2b3c7fee4ae73392b855b856e71880fbba581
Andrea Privitera <andrea.privitera@7gen.com>
2023-11-14 15:50:49 -0800

    used layout properly

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -7,13 +7,21 @@ export default function AboutLayout({
 }: {
     children: React.ReactNode;
 }) {
+    function getDate() {
+        return new Date().toLocaleDateString();
+    }
+    function GetTitle() {
+        return (
+            <h1>Daily post of { getDate() }</h1>
+        );
+    }
     return (
 
         <>
-        <article>
-            <h1></h1>
-            <div className={`${lusitana.className} antialiased `}>{children}</div>
-        </article>
+            <article>
+                <GetTitle />
+                <div className={`${lusitana.className} antialiased `}>{children}</div>
+            </article>
             {/* No errors */}
         </>
 
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,10 +2,10 @@ export default function About() {
     function getDate() {
         return new Date().toLocaleDateString();
     }
+
     return (
         <>
             <article>
-                <h1>Daily post of {getDate()}</h1>
                 <div>Welcome! Today is a beautiful day</div>
                 {/* No errors */}
             </article>
