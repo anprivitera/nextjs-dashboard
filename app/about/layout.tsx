2e380e05c46648110bd9cf037e4b41c16fd7cdff
Andrea Privitera <andrea.privitera@7gen.com>
2023-11-14 10:55:13 -0800

    changing function name does not fix the error

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -2,14 +2,14 @@ import '@/app/ui/global.css';
 import { lusitana } from '@/app/ui/fonts';
 
 
-export default function RootLayout({
+export default function aboutLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
         <html lang="en">
-            <body className={`${lusitana.className} antialiased `}>  {children}</body>
+            <body className={`${lusitana.className} antialiased `}>{children}</body>
             {/* Prop `className` did not match. Server: "__className_e66fe9 antialiased" Client: "__className_712214 antialiased */}
         </html>
     );
