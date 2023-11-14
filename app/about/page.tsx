0813d1d9614b4bfaa9f8b9bb3db0622d621e80af
Andrea Privitera <andrea.privitera@7gen.com>
2023-11-14 10:21:29 -0800

    removed unused useState

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react';
-
 export default function About() {
     function getDate() {
         return new Date().toLocaleDateString();
     }
     return (
         <>
-            <h1>Welcome! Today is { getDate() }</h1>
+            <h1>Welcome! Today is {getDate()}</h1>
             <h2>Here are some links to get you started:</h2>
         </>
     )
